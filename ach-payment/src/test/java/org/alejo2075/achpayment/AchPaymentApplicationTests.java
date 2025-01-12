425588422f1ca472fd1bb8f40c60b0bffa5a5992
Alejo2075 <alejsant75@gmail.com>
2025-01-11 19:32:36 -0600

    add ACH Payment module

---
+++ ach-payment/src/test/java/org/alejo2075/achpayment/AchPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.achpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
