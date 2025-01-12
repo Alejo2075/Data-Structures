0d4a3c69db0a4e81831e5defe28728df5f76a95e
Alejo2075 <alejsant75@gmail.com>
2025-01-12 15:32:28 -0600

    structure project

---
+++ src/main/java/org/alejo2075/cardpaymentprocessorr/CardPaymentProcessorApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.cardpaymentprocessorr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardPaymentProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardPaymentProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alejo2075/cardpaymentprocessorr/CardPaymentProcessorrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.cardpaymentprocessorr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardPaymentProcessorrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
