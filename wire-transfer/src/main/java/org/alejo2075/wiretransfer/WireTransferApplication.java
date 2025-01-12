a77a39c7af2e256ea8cbdf86a95fc26a5e397819
Alejo2075 <alejsant75@gmail.com>
2025-01-11 19:37:03 -0600

    add Wire Transfer module

---
+++ wire-transfer/src/main/java/org/alejo2075/wiretransfer/WireTransferApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.wiretransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WireTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WireTransferApplication.class, args);
+    }
+
+}
