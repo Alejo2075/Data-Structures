d79e7598f9371808d6c6a28814c3ae54c338ccc9
Alejo2075 <alejsant75@gmail.com>
2025-01-11 20:01:04 -0600

    add Config Server

---
+++ config-server/src/main/java/org/alejo2075/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
