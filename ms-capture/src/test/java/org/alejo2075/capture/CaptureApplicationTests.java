4a5888e273a57e4f1d41b01f66e39653c2eb1418
Alejo2075 <alejsant75@gmail.com>
2025-01-12 22:40:15 -0600

    structure project

---
+++ ms-capture/src/test/java/org/alejo2075/capture/CaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.capture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ms-chargeback/src/test/java/org/alejo2075/chargeback/ChargebackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.chargeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
